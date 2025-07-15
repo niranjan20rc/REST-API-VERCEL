f41c0faf8c585fbc60f350440b8e5da381d54977
niranjanrc20 <niranjanrc20@gmail.com>
2025-07-15 10:54:40 +0530

    first

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
+// api/index.js
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.get("/get",(req,res)=>{
-    res.send("GET")
-})
-app.post("/post",(req,res)=>{
-    res.send("POST")
-})
-app.put("/put",(req,res)=>{
-    res.send("PUT");
-})
-app.del("/delete",(req,res)=>{
-    res.send("DELETE");
-})
-app.listen(5000,()=>{console.log("server running")});
+
+app.get('/get', (req, res) => res.send('GET'));
+app.post('/post', (req, res) => res.send('POST'));
+app.put('/put', (req, res) => res.send('PUT'));
+app.delete('/delete', (req, res) => res.send('DELETE'));
+
+module.exports = app;
